beebc0cc8ff899ef92da2d58c3ea82383b1a7d40
sibraun <sibraun@3DLABOR13.in.fhkn.de>
2013-10-18 16:43:23 +0200

    test

--- de.htwg.se.mastermind/src/de/htwg/se/mastermind/mastermind.java
+++ de.htwg.se.mastermind/src/de/htwg/se/mastermind/mastermind.java
@@ -3,5 +3,8 @@ package de.htwg.se.mastermind;
 public class mastermind {
 	public static void main (String[] args) {
 		System.out.println("Unser Spiel heiﬂt: MASTERMIND");
+		int i = 0;
+		i++;
+		System.out.println(i);
 	}
 }
