c6980b9ab79f36951928592237862f62afc68bc4
sibraun <sibraun@3DLABOR13.in.fhkn.de>
2013-10-18 17:14:10 +0200

    add JUnit4

---
+++ de.htwg.se.mastermind/src/de/htwg/se/mastermind/testfile.java
@@ -0,0 +1,14 @@
+package de.htwg.se.mastermind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testfile {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
