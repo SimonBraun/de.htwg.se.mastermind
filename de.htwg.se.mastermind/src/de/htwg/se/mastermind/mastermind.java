271c0de1da37b56a147f5ff143cafe94c302961e
SimonBraun <sibraun@htwg-konstanz.de>
2013-10-25 12:49:10 +0200

    update main class

--- de.htwg.se.mastermind/src/de/htwg/se/mastermind/mastermind.java
+++ de.htwg.se.mastermind/src/de/htwg/se/mastermind/mastermind.java
@@ -3,9 +3,6 @@ package de.htwg.se.mastermind;
 public class mastermind {
 	public static void main (String[] args) {
 		System.out.println("Unser Spiel heiﬂt: MASTERMIND");
-		int i = 0;
-		i++;
-		System.out.println(i);
-		//SERVUS!!!!!!!!!!!!!!!
+				
 	}
 }
