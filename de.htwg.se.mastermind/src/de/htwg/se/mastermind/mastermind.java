8e946794e38db71fe5c1f7d36bc5d81aa4095f25
DennisMoosmann <dennis.moosmann@gmx.de>
2013-11-15 13:13:46 +0100

    New Start

--- de.htwg.se.mastermind/src/de/htwg/se/mastermind/mastermind.java
+++ de.htwg.se.mastermind/src/de/htwg/se/mastermind/mastermind.java
@@ -6,8 +6,6 @@ public class mastermind {
 	
 	public static void main (String[] args) {
 		
-		Grid g = new Grid();
-		g.setCellValue(0, 0, "yl");
-		System.out.println(g.getCellValue(0, 0));
+		
 	}
 }
--- de.htwg.se.mastermind/test/de/htwg/se/mastermind/TextUITest.java
+++
@@ -1,9 +0,0 @@
-package de.htwg.se.mastermind;
-
-import de.htwg.se.mastermind.tui.TextUI;
-
-
-
-public class TextUITest {
-	private TextUI text1;
-}
