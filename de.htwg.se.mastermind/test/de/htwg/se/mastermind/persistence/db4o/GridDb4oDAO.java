ec729ad035f31636641f3cd2d6f92c19d020bae3
DennisMoosmann <dennis.moosmann@gmx.de>
2015-04-08 12:31:38 +0200

    db4o

---
+++ de.htwg.se.mastermind/test/de/htwg/se/mastermind/persistence/db4o/GridDb4oDAO.java
@@ -0,0 +1,13 @@
+package de.htwg.se.mastermind.persistence.db4o;
+
+import com.db4o.ObjectContainer;
+import com.db4o.query.Predicate;
+import com.db4o.Db4oEmbedded;
+
+public class GridDb4oDAO {
+    private ObjectContainer db;
+
+    public GridDb4oDAO() {
+        db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), "grid.data");
+    }
+}
