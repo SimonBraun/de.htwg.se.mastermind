b0baa1ea2114446a839a7373b20bd4f0576794c1
Andreas Maier <andmaier@htwg-konstanz.de>
2015-04-24 01:51:13 +0200

    kein bock mehr auf git... btw. added missing file

---
+++ de.htwg.se.mastermind/src/de/htwg/se/mastermind/model/Colors.java
@@ -0,0 +1,21 @@
+package de.htwg.se.mastermind.model;
+
+public class Colors {
+	
+	private final String BASIC = "basic";
+	private final String COUNTRY = "country";
+	
+	private static final String [] basicColors = {"rd", "bl", "gr", "yl", "or", "pu", "pk"};	
+	private static final String [] countryColors = {"us", "de", "ch", "ru", "es", "fr", "gb"};
+	
+	public String [] getColor(String color){
+		if(color == BASIC){
+			return basicColors;
+		}
+		else if(color == COUNTRY){
+			return countryColors;
+		}
+		else return basicColors;
+	}
+ 
+}
