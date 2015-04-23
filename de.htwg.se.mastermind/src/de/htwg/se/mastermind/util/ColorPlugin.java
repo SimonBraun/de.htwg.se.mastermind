351fa3f193dd7970d689f0c7320ef79b02fae2c9
Andreas Maier <andmaier@htwg-konstanz.de>
2015-04-24 01:21:18 +0200

    another plugin stuff for google guice

---
+++ de.htwg.se.mastermind/src/de/htwg/se/mastermind/util/ColorPlugin.java
@@ -0,0 +1,19 @@
+package de.htwg.se.mastermind.util;
+
+public class ColorPlugin implements Plugin{
+
+	//TBD
+	
+	@Override
+	public String getMenuEntry() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public char getMenuKey() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ de.htwg.se.mastermind/src/de/htwg/se/mastermind/util/Plugin.java
@@ -0,0 +1,7 @@
+package de.htwg.se.mastermind.util;
+
+public interface Plugin {
+	//TBD
+	public String getMenuEntry();
+	public char getMenuKey();
+}
